313189c8f8d8e2069b5d1dd19b360c3fcde8e345
sanabarakat <sbarakat.ieu2021@student.ie.edu>
2023-10-14 15:36:11 +0200

    modifications

--- iebank_api/routes.py
+++ iebank_api/routes.py
@@ -9,30 +9,46 @@ def hello_world():
 
 @app.route('/skull', methods=['GET'])
 def skull():
-    return 'Hi! This is the BACKEND SKULL! 💀'
+    app.logger.debug('Route /skull GET called')
+    text = 'Hi! This is the BACKEND SKULL! 💀 '
+    text = text +'<br/>Database URL:' + db.engine.url.database
+    if db.engine.url.host:
+        text = text +'<br/>Database host:' + db.engine.url.host
+    if db.engine.url.port:
+        text = text +'<br/>Database port:' + db.engine.url.port
+    if db.engine.url.username:
+        text = text +'<br/>Database user:' + db.engine.url.username
+    if db.engine.url.password:
+        text = text +'<br/>Database password:' + db.engine.url.password
+    return text
 
 
 @app.route('/accounts', methods=['POST'])
 def create_account():
+    app.logger.debug('Route /accounts POST called')
     name = request.json['name']
     currency = request.json['currency']
-    account = Account(name, currency)
+    country = request.json['country']
+    account = Account(name, currency,country)
     db.session.add(account)
     db.session.commit()
     return format_account(account)
 
 @app.route('/accounts', methods=['GET'])
 def get_accounts():
+    app.logger.debug('Route /accounts GET')
     accounts = Account.query.all()
     return {'accounts': [format_account(account) for account in accounts]}
 
 @app.route('/accounts/<int:id>', methods=['GET'])
 def get_account(id):
+    app.logger.debug('Route /accounts GET called with id: ' + str(id))
     account = Account.query.get(id)
     return format_account(account)
 
 @app.route('/accounts/<int:id>', methods=['PUT'])
 def update_account(id):
+    app.logger.debug('Route /accounts PUT called with id: ' + str(id))
     account = Account.query.get(id)
     account.name = request.json['name']
     db.session.commit()
@@ -40,6 +56,7 @@ def update_account(id):
 
 @app.route('/accounts/<int:id>', methods=['DELETE'])
 def delete_account(id):
+    app.logger.debug('Route /accounts DELETE called with id: ' + str(id))
     account = Account.query.get(id)
     db.session.delete(account)
     db.session.commit()
@@ -52,6 +69,7 @@ def format_account(account):
         'account_number': account.account_number,
         'balance': account.balance,
         'currency': account.currency,
+        'country': account.country,
         'status': account.status,
         'created_at': account.created_at
     }
